d16f648bfe172c3a09dcefbd1af8b792ca97420d
Rahul Raman <rahulraman0108@gmail.com>
2020-12-22 10:30:45 +0530

    New scene add
    Just a new scene with more stable functionable camera for the time.

---
+++ Assets/Scripts/FreeLookInput.cs
@@ -0,0 +1,23 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class FreeLookInput : MonoBehaviour
+{
+        public CinemachineFreeLook fl;
+        
+        private Mouse _Mouse;
+
+        private void OnEnable()
+        {
+                _Mouse = Mouse.current;
+        }
+
+        private void Update()
+        {
+                var d = _Mouse.delta;
+                fl.m_XAxis.m_InputAxisValue = d.x.ReadValue();
+                fl.m_YAxis.m_InputAxisValue = d.y.ReadValue();
+        }
+}
