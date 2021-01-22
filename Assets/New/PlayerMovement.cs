5160de6a9560fe88acf859eea6eaf61ee8d72998
Kayakaga <baijidevelop@gmail.com>
2021-01-23 01:49:20 +0200

    Fixed the movement
    All that's left is the 3 gravity levels and some polish on the movement and camera

--- Assets/New/PlayerMovement.cs
+++ Assets/New/PlayerMovement.cs
@@ -5,34 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     /* TODO
-     *  1. Face only camera
-     *  2. Walk in facing direction
      *  3. Walk on planets
      */
     Rigidbody rb;
-    float speed = 10f;
+    [SerializeField]float speed = 1f;
     Vector3 target;
-    GameObject cam;
+    [SerializeField]GameObject cam;
+    [SerializeField] float rotationAcc = 1f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        cam = GameObject.FindGameObjectWithTag("MainCamera");
     }
     private void Update()
     {
-        target = cam.transform.position;
-        Vector3 direction = (transform.position - target).normalized;
-        transform.LookAt(new Vector3(direction.x, 0, direction.z));
+        Vector3 localTarget = -transform.InverseTransformPoint(cam.transform.position);
+
+        float angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+
+        Vector3 eulerAngleVelocity = new Vector3(0, angle, 0);
+        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * rotationAcc);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
     }
     private void FixedUpdate()
     {
-
-
-
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rb.velocity += new Vector3(horizontal * speed * Time.fixedDeltaTime, 0, vertical * speed * Time.fixedDeltaTime);
+        rb.velocity += transform.right * horizontal * speed;
+        rb.velocity += transform.forward * vertical * speed;
 
     }
 }
--- Assets/Scripts/ConjureBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class ConjureBase : ScriptableObject
-{
-    public abstract void DoBehaviour();
-    public abstract void Timeout(PooledMonoBehaviour instance);
-}
